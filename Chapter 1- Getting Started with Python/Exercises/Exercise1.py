7917d9297ff9f1939e8110ab8e22c7e94e6a0832
sohailx-12 <159238487+sohailx-12@users.noreply.github.com>
2024-02-28 10:50:21 +0400

    Update Exercise1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -6,3 +6,4 @@ Twinkle, twinkle, little star,
 Twinkle, twinkle, little star, 
 	How I wonder what you are
        ''')
+# The triple''' is used so that the a new line is added to the text automatically without putting extra code
