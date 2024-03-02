f98aed2a775852e3552ff16286e8a58314f3ebb8
sohailx-12 <159238487+sohailx-12@users.noreply.github.com>
2024-03-02 11:51:16 +0400

    Update Exercise3.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise3.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise3.py
@@ -1,1 +1,5 @@
-
+# We import date and time from the system
+import datetime
+# use today as a varaible and use the datetime now function
+Today= datetime.datetime.now()
+print("date and time",Today)
