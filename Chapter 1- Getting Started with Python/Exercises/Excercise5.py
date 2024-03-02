9011bfc92bf0ce324988130178a26ce899cd65a2
sohailx-12 <159238487+sohailx-12@users.noreply.github.com>
2024-03-02 11:54:02 +0400

    Update Excercise5.py

--- Chapter 1- Getting Started with Python/Exercises/Excercise5.py
+++ Chapter 1- Getting Started with Python/Exercises/Excercise5.py
@@ -1,1 +1,8 @@
-
+# import math so we can import the pi funct
+import math
+from math import pi
+# use input function to take response from user 
+radius= float(input("Enter the radius of the circle:"))
+# under area variable we enter the forumla which is shown below
+area= pi*radius**2
+print("The area of the circle is:",area)
