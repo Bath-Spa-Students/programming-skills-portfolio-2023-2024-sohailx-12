5ea1786ae6cd7e8249d39f8f55c215356dd30186
sohailx-12 <159238487+sohailx-12@users.noreply.github.com>
2024-03-02 11:53:21 +0400

    Update Excercise4.py

--- Chapter 1- Getting Started with Python/Exercises/Excercise4.py
+++ Chapter 1- Getting Started with Python/Exercises/Excercise4.py
@@ -1,1 +1,7 @@
-
+#3 strings are used in the below code
+device1 ="mobile phone,"
+device2 ="laptops"
+device3 =",camera."
+# here the ++ is used to combine the strings
+device= "Devices commonly used:" + device1+""+device2+""+device3
+print(device)
