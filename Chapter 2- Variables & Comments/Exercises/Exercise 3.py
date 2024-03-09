a0fdd1069950a9a9e1cccabc3704b9094346ed14
sohailx-12 <159238487+sohailx-12@users.noreply.github.com>
2024-03-09 13:18:53 +0400

    Create Exercise 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 3.py
@@ -0,0 +1,10 @@
+name=" \t ahmed \n "
+
+print(name)
+# printed with normal alignment
+print(name.lstrip())
+# printed with lef alginment due to l strip command
+print(name.rstrip())
+# printed with right alignment due to r strip command
+print(name.strip())
+# name is just strip
