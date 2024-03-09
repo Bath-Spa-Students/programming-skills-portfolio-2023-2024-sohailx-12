379a02e93612bcc270d1ab12234bfc1f5d547924
sohailx-12 <159238487+sohailx-12@users.noreply.github.com>
2024-03-09 13:20:05 +0400

    Create Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 5.py
@@ -0,0 +1,8 @@
+price_of_usb_stick= 6
+total_amount=50
+
+number_of_sticks_bought= total_amount // price_of_usb_stick
+reaming_amount=  total_amount % price_of_usb_stick
+
+print (f"She will be able to buy {number_of_sticks_bought} sticks")
+print(f"she will have {reaming_amount}pounds")
