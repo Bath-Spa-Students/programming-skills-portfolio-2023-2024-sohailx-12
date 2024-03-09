b8faf33c907912837640199303630ce6873c8b7a
sohailx-12 <159238487+sohailx-12@users.noreply.github.com>
2024-03-09 13:19:30 +0400

    Create Exercise 4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 4.py
@@ -0,0 +1,4 @@
+Fav_number=99
+number=f"My favourite number is {Fav_number}."
+# fav_number var is created and the a new varaible is created to add a steence and conctudate the variable 
+print(number)
